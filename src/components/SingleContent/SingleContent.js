ab47ca5f21107a1687b4c861e4c92bf90c2900af
harysreza <harysreza21@gmail.com>
2021-12-03 18:36:11 +0700

    edit hover animation

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -7,7 +7,9 @@ const SingleContent = ({ id, poster, title, date, media_type, vote_average }) =>
   return (
     <ContentModal media_type={media_type} id={id}>
       <Badge badgeContent={vote_average} color={vote_average > 6 ? "primary" : "error"} />
-      <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+      <div className="zoom">
+        <img src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+      </div>
       <b className="title">{title}</b>
       <span className="subtitle">{media_type === "tv" ? "TV Series" : "Movie"}</span>
     </ContentModal>
