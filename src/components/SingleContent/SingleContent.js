ca23bec68e9980acd79c77156499052bbc6da7ed
harysreza <harysreza21@gmail.com>
2021-12-05 17:25:24 +0700

    add truncate string function

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -3,6 +3,11 @@ import Badge from "@mui/material/Badge";
 import "./SingleContent.css";
 import ContentModal from "../ContentModal/ContentModal";
 
+let stringTruncate = function (str, length) {
+  let dots = str.length > length ? "..." : "";
+  return str.substring(0, length) + dots;
+};
+
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
     <ContentModal media_type={media_type} id={id}>
@@ -10,7 +15,7 @@ const SingleContent = ({ id, poster, title, date, media_type, vote_average }) =>
       <div className="zoom">
         <img src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
       </div>
-      <b className="title">{title}</b>
+      <b className="title">{stringTruncate(title, 20)}</b>
       <span className="subtitle">{media_type === "tv" ? "TV Series" : "Movie"}</span>
     </ContentModal>
   );
